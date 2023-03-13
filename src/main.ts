c33f4c120d31a0ed8edbc3f848c48565afa1c63e
ilya07092000 <ischenko2012@gmail.com>
2023-03-13 10:18:55 +0200

    feat: autobind decorator

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	const adjDesriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+
+	return adjDesriptor;
+}
+
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
@@ -25,12 +39,13 @@ class ProjectInput {
 		this.attach();
 	}
 
+	@autobind
 	private submitHandler(e: SubmitEvent) {
 		e.preventDefault();
 	}
 
 	private configure() {
-		this.element.addEventListener('submit', this.submitHandler.bind(this));
+		this.element.addEventListener('submit', this.submitHandler);
 	}
 
 	private attach() {
