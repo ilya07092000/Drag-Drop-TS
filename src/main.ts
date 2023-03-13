05b685d6e9ca89633f5152f1151dbac92905b9b1
ilya07092000 <ischenko2012@gmail.com>
2023-03-13 10:13:16 +0200

    feat: added fields with submit handler

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.querySelector('#project-input')!;
@@ -12,8 +15,22 @@ class ProjectInput {
 			true
 		);
 		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = 'user-input';
 
+		this.titleInputElement = this.element.querySelector('#title')!;
+		this.descriptionInputElement = this.element.querySelector('#description')!;
+		this.peopleInputElement = this.element.querySelector('#people')!;
+
+		this.configure();
 		this.attach();
+	}
+
+	private submitHandler(e: SubmitEvent) {
+		e.preventDefault();
+	}
+
+	private configure() {
+		this.element.addEventListener('submit', this.submitHandler.bind(this));
 	}
 
 	private attach() {
