6cf9e95e95aed0891971379f72e4fc9fb5c26641
ilya07092000 <ischenko2012@gmail.com>
2023-03-15 10:33:57 +0200

    feat: validation interface

---
+++ src/types/validation.ts
@@ -0,0 +1,11 @@
+// validation
+interface Validatable {
+	value: string | number;
+	required?: boolean;
+	minLength?: number;
+	maxLength?: number;
+	min?: number;
+	max?: number;
+}
+
+export type { Validatable };
