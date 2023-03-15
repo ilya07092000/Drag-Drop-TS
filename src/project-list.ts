b5426971ac401c795267043ac1066e38afa38266
ilya07092000 <ischenko2012@gmail.com>
2023-03-15 10:34:26 +0200

    moved project list to dedicated folder

---
+++ src/project-list.ts
@@ -0,0 +1,51 @@
+import { projectState } from './project-state';
+
+class ProjectList {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLElement;
+	assignedProjects: any[] = [];
+
+	constructor(private type: 'active' | 'finished' = 'active') {
+		this.templateElement = document.querySelector('#project-list')!;
+		this.hostElement = document.querySelector('#app')!;
+
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		this.element = importedNode.firstElementChild as HTMLElement;
+		this.element.id = `${type}-projects`;
+
+		projectState.addListener((projects: any[]) => {
+			this.assignedProjects = projects;
+			this.renderProjects();
+		});
+		this.attach();
+		this.renderContent();
+	}
+
+	private renderProjects() {
+		const listEl = document.getElementById(`${this.type}-projects-list`)!;
+		for (const projectItem of this.assignedProjects) {
+			const listItem = document.createElement('li');
+			listItem.textContent = projectItem.title;
+
+			listEl.appendChild(listItem);
+		}
+	}
+
+	private renderContent() {
+		const listId = `${this.type}-projects-list`;
+		this.element.querySelector('ul')!.id = listId;
+		this.element.querySelector(
+			'h2'
+		)!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement('beforeend', this.element);
+	}
+}
+
+export default ProjectList;
