3f6983537015a30c0d9d5177dc9b0bb63a06cb14
ilya07092000 <ischenko2012@gmail.com>
2023-03-16 10:59:13 +0200

    feat: project item persons getter

--- src/project-item.ts
+++ src/project-item.ts
@@ -4,6 +4,14 @@ import Project from './project';
 class ProjectItem extends ComponentBase<HTMLUListElement, HTMLLIElement> {
 	private project: Project;
 
+	get persons() {
+		if (this.project.people === 1) {
+			return '1 person';
+		} else {
+			return `${this.project.people} persons`;
+		}
+	}
+
 	constructor(hostId: string, project: Project) {
 		super('single-project', hostId, false, project.id.toString());
 		this.project = project;
@@ -15,8 +23,7 @@ class ProjectItem extends ComponentBase<HTMLUListElement, HTMLLIElement> {
 	configure(): void {}
 	renderContent(): void {
 		this.element.querySelector('h2')!.textContent = this.project.title;
-		this.element.querySelector('h3')!.textContent =
-			this.project.people.toString();
+		this.element.querySelector('h3')!.textContent = `${this.persons} assigned`;
 		this.element.querySelector('p')!.textContent = this.project.description;
 	}
 }
